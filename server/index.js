00541a58395d50dd93c2eea4e8a0f48d2cc8357e
mahimagontalwar <mahimagontalwar1209@gmail.com>
2024-09-07 18:11:06 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const userRoutes=require('./routes/userRoutes');
 const movieRoutes=require('./routes/movieRoutes');
 const theatreRoutes=require('./routes/theatreRoutes');
 const showRoutes=require('./routes/showRoutes');
-//const bookingRoute = require('./routes/bookingRoute');
+const bookingRoute = require('./routes/bookingRoute');
 require('dotenv').config();
 
 const dburl="mongodb+srv://mahima:NkvqU5ZYQyRlNaSo@cluster0.a8wuiyh.mongodb.net/scaler?retryWrites=true&w=majority&appName=Cluster0";
@@ -18,7 +18,7 @@ app.use('/api/users',userRoutes);
 app.use('/api/movies',movieRoutes);
 app.use('/api/theatres',theatreRoutes);
 app.use('/api/shows',showRoutes);
-//app.use('/api/booking', bookingRoute)
+app.use('/api/booking', bookingRoute)
 app.listen(8081,()=>{
     console.log("server is connected");
 })
